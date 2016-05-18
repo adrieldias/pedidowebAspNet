920c3035559bc8adde432b431a8b2925e9ef2b03
marloncemapa <marlon@cemapa.com>
2016-05-18 15:57:34 -0300

    Cálculo do status do pedido para empresas que alteram valor unitário

--- PedidoWeb/PedidoWeb/Controllers/Negocio/StatusPedido.cs
+++ PedidoWeb/PedidoWeb/Controllers/Negocio/StatusPedido.cs
@@ -15,13 +15,28 @@ namespace PedidoWeb.Controllers.Negocio
         /// <param name="p">Pedido</param>
         /// <returns>Status de pedido</returns>
         public string CalculaStatus(Pedido p)
-        {            
+        {    
+            var empresa = db.Empresas.Find(p.CodEmpresa);
             foreach(var item in p.Itens)
             {
-                var produtoPadrao = db.Produtoes.Find(item.ProdutoID);
-
-                if (item.PercentualDesconto > produtoPadrao.PercDescontoMaximo)
-                    return "EM ANALISE";                
+                if (!empresa.AlteraValorUnitario)
+                {
+                    var produtoPadrao = db.Produtoes.Find(item.ProdutoID);
+                    if (item.PercentualDesconto > produtoPadrao.PercDescontoMaximo)
+                        return "EM ANALISE";
+                }
+                else                
+                {
+                    var produtoPadrao = db.Produtoes.Find(item.ProdutoID);
+                    if(item.ValorUnitario < produtoPadrao.PrecoVarejo)
+                    {
+                        var percDesc = 100 - (item.ValorUnitario * 100 / produtoPadrao.PrecoVarejo);
+                        if (item.PercentualDesconto != null && item.PercentualDesconto > 0)
+                            percDesc += Convert.ToDecimal(item.PercentualDesconto);
+                        if (percDesc > produtoPadrao.PercDescontoMaximo)
+                            return "EM ANALISE";
+                    }
+                }
             }
             return "APROVADO";
         }
